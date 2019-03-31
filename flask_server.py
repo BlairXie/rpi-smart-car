9a14939263305ad0e87deafdecb1c91b45954d6b
Blair <1263815597@qq.com>
2019-03-31 23:38:05 +0800

    port

--- flask_server.py
+++ flask_server.py
@@ -28,4 +28,4 @@ def button(direction):
     return redirect(url_for('show_index'))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0',port=8000, debug=True, threaded=True)
