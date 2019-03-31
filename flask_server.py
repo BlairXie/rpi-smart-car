9a233bd2a9cc749882c294ee780e0dc912f7848c
blair <1263815597@qq.com>
2019-03-31 23:31:21 +0800

    new & modified

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask
-from flask import abort, redirect, url_for, render_template
+from flask import Flask, abort, redirect, url_for, render_template
+from flask import Response
+from camera_pi import Camera
 # import ultrasonic
 app = Flask(__name__)
 
@@ -8,10 +9,23 @@ app = Flask(__name__)
 def show_index():
   return render_template('flask_server.html')
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/<direction>')
 def button(direction):
 
     return redirect(url_for('show_index'))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True, threaded=True)
