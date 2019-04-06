43a6ea8c31139dbf39a2b5772d7bd368905e78f9
blair <1263815597@qq.com>
2019-04-06 15:36:54 +0800

    uft8

--- motor.py
+++ motor.py
@@ -13,8 +13,8 @@ GPIO.setup(GPIO_IN1, GPIO.OUT)
 GPIO.output(GPIO_IN2,True)
 GPIO.output(GPIO_IN1,True)
 
-p = GPIO.PWM(GPIO_IN1, 400) # channel=? frequency=50Hz（需要修改高电平引脚）
-p = GPIO.PWM(GPIO_IN2, 400) # channel=? frequency=50Hz（需要修改高电平引脚）
+p = GPIO.PWM(GPIO_IN1, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
+p = GPIO.PWM(GPIO_IN2, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
 p.start(0) #to start PWM
 
 try:
