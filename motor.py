851ef66aec53b269326ecb8bb3652d894828aae8
blair <1263815597%40qq.com>
2019-04-06 21:51:57 +0800

    update

--- motor.py
+++ motor.py
@@ -24,6 +24,8 @@ p1 = GPIO.PWM(ENA, 200) # channel=? frequency=50Hz（需要修改高电平引脚
 p2 = GPIO.PWM(ENB, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
 p1.start(41) #to start PWM
 p2.start(48)
+pre_distance = 0
+turn_flag = 1 # 1代表 turn left , 0 代表 turn right
 
 def forward():
     GPIO.output(GPIO_IN1,False)
@@ -44,13 +46,23 @@ def turnRight():
     GPIO.output(GPIO_IN4,True)
 
 def forward_avoid_obstacle():
+    global pre_distance
+    global turn_flag
     dist = distance()
     print("Measured Distance = {:.2f} cm".format(dist))
     if(dist < 25 ):
-        turnLeft()
+        if dist < pre_distance:
+            turn_flag = not turn_flag
+        if turn_flag:
+            turnLeft()
+        else:
+            turnRight()
+        pre_distance = dist
     else:
+        pre_distance =0
+        turn_flag = 1
         forward()
-    time.sleep(0.05)
+    time.sleep(0.01)
 
 
 try:
