36d751c00d0d068d08f287ea64f41199a15a9b1f
blair <1263815597%40qq.com>
2019-04-06 18:52:06 +0800

    turn

--- motor.py
+++ motor.py
@@ -21,24 +21,41 @@ GPIO.output(GPIO_IN2,True)
 GPIO.output(GPIO_IN3,True)
 GPIO.output(GPIO_IN4,False)
 
-p1 = GPIO.PWM(GPIO_IN2, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
-p2 = GPIO.PWM(GPIO_IN3, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
-p1.start(40) #to start PWM
-p2.start(50)
-# def turnLeft(distance):
-#     if distance <= 5:
+p2 = GPIO.PWM(GPIO_IN2, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
+p3 = GPIO.PWM(GPIO_IN3, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
+p2.start(40) #to start PWM
+p3.start(50)
+def turnLeft(dist):
+    if distance <= 10:
+        p2.stop()
+        GPIO.output(GPIO_IN1,True)
+        GPIO.output(GPIO_IN2,False)
+        p1 = GPIO.PWM(GPIO_IN1, 200)
+        p1.start(40)
+
+        time.sleep(5)
+
+        p1.stop()
+        GPIO.output(GPIO_IN1,False)
+        GPIO.output(GPIO_IN2,True)
+        p2 = GPIO.PWM(GPIO_IN2, 200) # channel=? frequency=50Hz（需要修改高电平引脚）
+        p2.start(40) #to start PWM
+    return 0
+
+
+
 
 try:
     while 1:
         dist = distance()
-        print("Measured Distance = {:.2f} cm".format(dist))
+        turnLeft(dist)
         time.sleep(1)
 
 
 
 # Reset by pressing CTRL + C
 except KeyboardInterrupt:
-    p1.stop()
     p2.stop()
+    p3.stop()
     print("Measurement stopped by User")
     GPIO.cleanup()
